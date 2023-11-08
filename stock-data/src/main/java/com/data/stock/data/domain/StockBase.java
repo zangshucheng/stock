52238dc6fd3ffff35e5f2931fa8c66fe842dcd8d
zangshucheng <867123517@qq.com>
2023-11-08 21:13:06 +0800

    daily price

--- stock-data/src/main/java/com/data/stock/data/domain/StockBase.java
+++ stock-data/src/main/java/com/data/stock/data/domain/StockBase.java
@@ -14,60 +14,72 @@ import lombok.Data;
 @TableName(value ="stock_base")
 @Data
 public class StockBase implements Serializable {
+    /**
+     * 股票代码
+     */
+    @TableId(value = "stock_code")
+    private String stockCode;
 
     /**
      * 自定义编码
      */
+    @TableField(value = "ts_code")
     private String tsCode;
 
     /**
      * 股票名称
      */
+    @TableField(value = "stock_name")
     private String stockName;
-
-    /**
-     * 股票代码
-     */
-    @TableId()
-    private String stockCode;
 
     /**
      * 所属区域
      */
+    @TableField(value = "area")
     private String area;
 
     /**
      * 所属行业
      */
+    @TableField(value = "industry")
     private String industry;
 
     /**
      * 股票全称
      */
+    @TableField(value = "full_name")
     private String fullName;
 
     /**
      * 英文名称
      */
+    @TableField(value = "en_name")
     private String enName;
 
     /**
      * 中文缩写
      */
+    @TableField(value = "cn_spell")
     private String cnSpell;
 
     /**
      * 市场类型（主板/创业板/科创板/CDR）
      */
+    @TableField(value = "market")
     private String market;
 
     /**
      * 上市状态 L上市 D退市 P暂停上市
      */
+    @TableField(value = "status")
     private String status;
 
     /**
      * 是否沪深港通标的，N否 H沪股通 S深股通
      */
+    @TableField(value = "is_hs")
     private String isHs;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
--- stock-data/src/main/java/com/data/stock/data/domain/StockDaily.java
+++ stock-data/src/main/java/com/data/stock/data/domain/StockDaily.java
@@ -1,80 +1,92 @@
 package com.data.stock.data.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
+import lombok.Data;
 
 /**
  * 
  * @TableName stock_daily
  */
-@Data
 @TableName(value ="stock_daily")
+@Data
 public class StockDaily implements Serializable {
-
     /**
-     * 主键id
+     * 
      */
-    @TableId(type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
      * 股票代码
      */
+    @TableField(value = "stock_code")
     private String stockCode;
 
     /**
      * 交易日期
      */
+    @TableField(value = "trade_date")
     private String tradeDate;
 
     /**
      * 开盘价
      */
+    @TableField(value = "open")
     private BigDecimal open;
 
     /**
      * 最高价
      */
+    @TableField(value = "high")
     private BigDecimal high;
 
     /**
      * 最低价
      */
+    @TableField(value = "low")
     private BigDecimal low;
 
     /**
      * 收盘价
      */
+    @TableField(value = "close")
     private BigDecimal close;
 
     /**
-     * 
+     * 上个交易日收盘价格
      */
+    @TableField(value = "pre_close")
     private BigDecimal preClose;
 
     /**
-     * 
+     * 价格变动幅度
      */
+    @TableField(value = "change_range")
     private BigDecimal changeRange;
 
     /**
-     * 
+     * 价格变动百分比
      */
+    @TableField(value = "pct_chg")
     private BigDecimal pctChg;
 
     /**
-     * 
+     * 成交总手数
      */
+    @TableField(value = "trade_volume")
     private BigDecimal tradeVolume;
 
     /**
-     * 
+     * 成交总额
      */
+    @TableField(value = "amount")
     private BigDecimal amount;
 
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
